5ec5ed377665d6f5e4e0b1fceb4a2db667677a31
Gorthi-Sarvani <sarvanilakshmidevi@gmail.com>
2022-11-29 13:52:42 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+ const express=require("express")
+ const app=express()
+ const cors=require("cors")
+ const bodyParser=require("body-parser")
+ const mongoose=require("mongoose")
+ const Register=require("./registerSchema.js")
+
+
+ const port=4000
+ app.use(bodyParser.urlencoded({
+    extended:true
+ }))
+ app.use(bodyParser.json())
+ app.use(cors())
+ mongoose.connect("mongodb+srv://sarvani:sarvani@cluster0.utd2xku.mongodb.net/firstdb?retryWrites=true&w=majority")
+ .then(()=>{
+    console.log("connection established")
+ })
+ .catch((err)=>{
+    console.log(err)
+ })
+ app.get("/",(req,res)=>{
+    console.log("dummy")
+ })
+ app.post("/register",(req,res)=>{
+    //const {email,passcode}=req.body;
+    const email="user1@gmail.com",passcode="7654"
+    const newFrontendUser=new Register({
+        username:email,
+        password:passcode
+    })
+    newFrontendUser.save()
+ })
+
+ app.listen(port,
+    ()=>console.log("server is running on port"))
+ 
